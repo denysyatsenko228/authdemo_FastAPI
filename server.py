0cf9b23bee7a9dadea1281e5ae25ec30d0e00e90
denysyatsenko228 <dayyz@bk.ru>
2025-03-18 21:41:48 +0200

    Added 'black'

--- server.py
+++ server.py
@@ -16,10 +16,11 @@ PASSWORD_SALT = "3df3e222b229c1c8c3d2a4af093ba2f42b9f34cd1ed5206a37a5f696253bb2f
 
 def sign_data(data: str) -> str:
     """Возвращает подписанные данные data"""
-    return hmac.new(SECRET_KEY.encode(),
-                    msg=data.encode(),
-                    digestmod=hashlib.sha256
-                    ).hexdigest().upper()
+    return (
+        hmac.new(SECRET_KEY.encode(), msg=data.encode(), digestmod=hashlib.sha256)
+        .hexdigest()
+        .upper()
+    )
 
 
 def get_username_from_signed_string(username_signed: str) -> Optional[str]:
@@ -31,7 +32,9 @@ def get_username_from_signed_string(username_signed: str) -> Optional[str]:
 
 
 def verify_password(username: str, password: str) -> bool:
-    password_hash = hashlib.sha256((password + PASSWORD_SALT).encode()).hexdigest().lower()
+    password_hash = (
+        hashlib.sha256((password + PASSWORD_SALT).encode()).hexdigest().lower()
+    )
     stored_password_hash = users[username]["password"].lower()
     return password_hash == stored_password_hash
 
@@ -40,13 +43,13 @@ users = {
     "alexey@gmail.com": {
         "name": "Алексей",
         "password": "693d120a92d9446bda7e03f942c5d8ff76e8ca56830954c30da8c6e0bfb31dc9",
-        "balance": 100_000
+        "balance": 100_000,
     },
     "petr@user.com": {
         "name": "Петр",
         "password": "8bb94412970f2cd44af10473eeb43173240da491082e96e01fba4963b5169563",
-        "balance": 20_000
-    }
+        "balance": 20_000,
+    },
 }
 
 
@@ -55,10 +58,10 @@ def index_page(username: Optional[str] = Cookie(default=None)):
     with open("templates/login.html", "r") as f:
         login_page = f.read()
     if not username:
-        return Response(login_page, media_type='text/html')
+        return Response(login_page, media_type="text/html")
     valid_username = get_username_from_signed_string(username)
     if not valid_username:
-        response = Response(login_page, media_type='text/html')
+        response = Response(login_page, media_type="text/html")
         response.delete_cookie(key="username")
         return response
 
@@ -68,31 +71,36 @@ def index_page(username: Optional[str] = Cookie(default=None)):
         response = Response(login_page, media_type="text/html")
         response.delete_cookie(key="username")
         return response
-    return Response(f"Привет, {users[valid_username]['name']}!<br />"
-                    f"Баланс: {users[valid_username]['balance']}",
-                    media_type="text/html")
+    return Response(
+        f"Привет, {users[valid_username]['name']}!<br />"
+        f"Баланс: {users[valid_username]['balance']}",
+        media_type="text/html",
+    )
 
 
-@app.post('/login')
+@app.post("/login")
 def process_login_page(data: dict = Body(...)):
     username = data["username"]
     password = data["password"]
     user = users.get(username)
     if not user or not verify_password(username, password):
         return Response(
-            json.dumps({
-                "success": False,
-                "message": "Я вас не знаю!"
-            }),
-            media_type="application/json")
+            json.dumps({"success": False, "message": "Я вас не знаю!"}),
+            media_type="application/json",
+        )
 
     response = Response(
-        json.dumps({
-            "success": True,
-            "message":  f"Привет, {user['name']}!<br /> Баланс: {user['balance']}"
-        }),
-        media_type="application/json")
+        json.dumps(
+            {
+                "success": True,
+                "message": f"Привет, {user['name']}!<br /> Баланс: {user['balance']}",
+            }
+        ),
+        media_type="application/json",
+    )
 
-    username_signed = base64.b64encode(username.encode()).decode() + "." + sign_data(username)
+    username_signed = (
+        base64.b64encode(username.encode()).decode() + "." + sign_data(username)
+    )
     response.set_cookie(key="username", value=username_signed)
     return response
